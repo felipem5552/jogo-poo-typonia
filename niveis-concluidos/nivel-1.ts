e5afeed12dd9ea419034263a5333e0d154848a9f
Felipe Mendes Alves <felipemendes5552@gmail.com>
2025-10-26 23:17:24 -0300

    Create Defensor class with two instances
    Implement the Defensor class with properties and constructor.

---
+++ niveis-concluidos/nivel-1.ts
@@ -0,0 +1,23 @@
+// Sua missão: Criar a classe Defensor e instanciar dois objetos
+// Exemplo de uso esperado:
+// const defensor1 = new Defensor("Arthur", "Guerreiro", 10);
+// const defensor2 = new Defensor("Morgana", "Mago", 12);
+
+class Defensor {
+    nome: string
+    tipo: string
+    ataque: number
+}
+
+     constructor(nome:string, tipo: string, ataque: number) {
+             this.nome = nome
+             this.tipo = tipo
+             this.ataque = ataque 
+}
+
+
+
+
+
+const defensor1 = new Defensor ("Felipe", "Ladino", 10);
+const defensor2 = new Defensor ("Elida", "Mago", 24);
